ce78c30f645854ebbd65c781609b187346c17805
Anish oli <olianish534@gmail.com>
2025-11-28 09:52:01 +0545

    Created blogs app and blog model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#media field
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
